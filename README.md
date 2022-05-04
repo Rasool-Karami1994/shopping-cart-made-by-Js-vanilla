83200cd5464e045a17092c9c535c15142c8223ac
rasool700 <rasoolkarami700@gmail.com>
2022-05-04 17:38:30 +0430

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Shop-Cart Project
+
+An application that simulate parts of an online shopping website shop cart.
+
+### Reflection
+
+Project goals included using technologies learned up until this point and familiarizing myself with documentation for new features.
+
+Originally I wanted to build an application that allowed users to add products to Shop-cart, Eliminate, increase or decrease the number and calculate the price of products in the cart and finally clear the cart or confirm it.
+
+I used vanilla JS and Css for building this project.
---
+++ products.js
@@ -0,0 +1,44 @@
+export const productsData = [
+  {
+    id: 1,
+    title: "Royal Twin Bed",
+    price: 720.2,
+    imageUrl: "./images/product-1.jpeg",
+  },
+  {
+    id: 2,
+    title: "Modern Twin Bed",
+    price: 800,
+    imageUrl: "./images/product-2.jpeg",
+  },
+  {
+    id: 3,
+    title: "classic Twin Bed",
+    price: 860,
+    imageUrl: "./images/product-3.jpeg",
+  },
+  {
+    id: 4,
+    title: "wooden Beds",
+    price: 500,
+    imageUrl: "./images/product-4.jpeg",
+  },
+  {
+    id: 5,
+    title: "Kitchen Desk",
+    price: 650,
+    imageUrl: "./images/product-5.jpeg",
+  },
+  {
+    id: 6,
+    title: "relax bed",
+    price: 890,
+    imageUrl: "./images/product-6.jpeg",
+  },
+  {
+    id: 7,
+    title: "Modern Armchairs",
+    price: 1200,
+    imageUrl: "./images/product-7.jpeg",
+  },
+];
